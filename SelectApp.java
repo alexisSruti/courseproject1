9904d80eb9ae902a46c81a74bfcaac67cae433dc
alexisSruti <31807530+alexisSruti@users.noreply.github.com>
2017-10-09 18:19:38 -0500

    Added file via upload

---
+++ SelectApp.java
@@ -0,0 +1,62 @@
+package net.sqlitetutorial;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class SelectApp {
+
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:Documents/College/Software Modeling and Design/StoreManagement.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(){
+        String sql = "SELECT ProductID, ProductName, Price FROM StoreManagement";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("ProductID") +  "\t" +
+                                   rs.getString("ProductName") + "\t" +
+                                   rs.getDouble("Price"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.selectAll();
+    }
+
+}
