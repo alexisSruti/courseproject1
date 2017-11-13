e3349951d64417e9eac03e44cf7561ea26fc9219
alexisSruti <31807530+alexisSruti@users.noreply.github.com>
2017-11-13 16:21:03 -0600

    Update User.java
    Added filename, password, username

--- User.java
+++ User.java
@@ -4,15 +4,24 @@ public class User implements Comparable {
   private String name;
   private int id;
   private boolean isManager;
+  private String username;
+  private String fileName;
+  private String password;
   public User() {
     name = "null";
     id = 0;
     isManager = false;
+    username = "null";
+    password = "null";
+    fileName = "null";
   }
-  public User(String nameIn, int idIn, boolean isManagerIn) {
+  public User(String nameIn, int idIn, boolean isManagerIn, String usernameIn, String passwordIn, String fileNameIn) {
     name = nameIn;
     id = idIn;
     isManager = isManagerIn;
+    username = usernameIn;
+    password = passwordIn;
+    fileName = fileNameIn;
   }
 
   //Getters
@@ -25,6 +34,15 @@ public class User implements Comparable {
   public boolean getIsManager() {
     return isManager;
   }
+  public String getUsername() {
+    return username;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public String getFilename() {
+    return fileName;
+  }
 
   //Setters
   public void setName(String nameIn) {
@@ -36,10 +54,19 @@ public class User implements Comparable {
   public void setIsManager(boolean isManagerIn) {
     this.isManager = isManagerIn;
   }
-
+  public void setUsername(String usernameIn) {
+    this.username = usernameIn;
+  }
+  public void setPassword(String passwordIn) {
+    this.password = passwordIn;
+  }
+  public void setFilename(String fileNameIn) {
+    this.fileName = fileNameIn;
+  }
   //toString method
   public String toString() {
-    String str = "Name: " + name + " ID: " + id + " isManager: " + isManager;
+    String str = "Name: " + name + " ID: " + id + " isManager: " + isManager + " Username: " + username +
+                  " Password: " + password + " File Name: " + fileName;
     return str;
   }
 
